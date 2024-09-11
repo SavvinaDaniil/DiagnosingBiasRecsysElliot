929b1f15046a51d862d150ab72363fee1307e93b
Savvina <savvina_daniil@hotmail.com>
2024-09-11 13:58:20 +0200

    Improve  widgets

--- lib/analysis_recommendations.py
+++ lib/analysis_recommendations.py
@@ -1,8 +1,7 @@
-import os, json
-import pandas as pd
-from lib import modelling_mf
+import pickle as pkl
 import numpy as np
-import pickle
+import pandas as pd
+from scipy.stats import mannwhitneyu
 from tqdm import tqdm
 
 import logging, os
@@ -11,160 +10,100 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import tensorflow as tf
 
 
+def highest_average(df_list, column_name = 'recommendation'):
+    highest_average = -(10**6)
+    highest_i = -1
+    for i in range(len(df_list)):
+        df = df_list[i]
+        mean = np.mean(df[column_name].values)
+        # print('mean', mean)
+        if mean > highest_average:
+            highest_average = mean
+            highest_i = i
+    # print(highest_average, highest_i)
+    return highest_average, highest_i
+    
+def mannwhitneyu_test(df_list, alt = 'greater', column_name = 'recommendation'):
+    # find the highest average 
+    
+    ha, hi = highest_average(df_list, column_name)
+    print('Highest version: ', hi, ha)
+    inds_df_list = list(range(len(df_list)))
+    to_test_inds = inds_df_list[:hi] + inds_df_list[hi+1:]
+    # print(to_test_inds)
+    df1 = df_list[hi]
+    pvalues = []
+    for ind in to_test_inds:
+        df2 = df_list[ind]
+        x = df1[column_name].values
+        y = df2[column_name].values
+        pvalue = mannwhitneyu(x,y, alternative = alt)[1]
+        pvalues.append(pvalue)
+    return [(to_test_inds[i],pvalues[i]) for i in range(len(pvalues))] # pvalues for all comparisons
 
 
-
-def process(dataset):
+def analyse(dataset):
     if dataset == 'fairbook' or dataset == 'ml1m':
-        process_real(dataset)
+        analyse_real(dataset)
     elif dataset == 'epinion':
         print('Currently unavailable.')
     elif dataset == 'synthetic':
-        process_synthetic()
+        analyse_synthetic()
     else:
         print("Error! This is not one of the available datasets.")
 
 
-def process_real(data):
-    algorithm = 'DMF'
-    batch_size = 1024
+def analyse_real(data_strategy):
 
-    user_col = "user" # the name of the column that includes the users
-    item_col = "item" # the name of the column that includes the items
-    predict_col="rating" # the name of the column that includes the interaction
-    if data == 'fairbook':
-        ratings = pd.read_csv("data/"+data+"/"+data+"_events.csv")
-    else: 
-        ratings = pd.read_csv(
-            'data/movielens_1m/ml1m_events.dat', header=None, sep="::", engine="python"
-        ).drop(3, axis=1)
-        ratings.columns = ["user", "item", "rating"]
-
-    all_items = set(ratings.item.unique())
-    final_recs_files = {}
-    for fold in range(1,6):
-        # Set locations of recommendations and best iteration files
-        recs_location = 'results/'+data+str(fold)+'/recs/'
-        best_iter_location = 'results/'+data+str(fold)+'/performance/'
-        
-        # Find all json files in the folder (they contain the best iterations).
-        bestmodelparams_files = [best_iter_location + pos_json for pos_json in os.listdir(best_iter_location) if pos_json.endswith('.json')]
-
-        # For every json find the mlp value and the equivalent best iteration and learning rate (which was set after hyperparameter tuning).
-        best_iters = {}
-        for file in bestmodelparams_files:
-            with open(file) as f:
-                d = json.load(f)
-            mlp = d[1]['configuration']['item_mlp']
-            best_iteration = d[1]['configuration']['best_iteration']
-            lr = d[1]['configuration']['lr'] # this was found in a previous step, not during training
-            best_iters[mlp] = (lr, best_iteration)
-            
-        # For every mlp value, find the file that contains the recommendations.
-        mlp_values = best_iters.keys()
-        
-        for mlp in mlp_values:
-            lr = str(best_iters[mlp][0]).replace('.','$')
-            bi = str(best_iters[mlp][1])
-            mlp = mlp.replace(',','-').replace('(','').replace(')','')
-            recs_file = recs_location + algorithm+'_seed=42_e=25_bs='+str(batch_size)+'_lr='+lr+'_umlp='+mlp+'_imlp='+mlp+'_negratio=5_reg=0$001_sim=cosine_it='+bi+'.tsv'
-            final_recs_files[fold,mlp] = recs_file
-        mlp_values = np.unique([x[1] for x in final_recs_files.keys()]) # change the format   
-
+    
+    mlp_values = ['64-32', '64-64'] # the different versions of the algorithm tested
+    algo_name = "DMF"
+    file_location = "metrics/" + algo_name + "/" +data_strategy+"/"
+    results = []
     for mlp in mlp_values:
-        metrics = []
-        
-        total_ndcg = 0.0
-        total_st_dev_20 = 0.0
-        total_pop_bias_10 = 0.0
-        total_precision = 0.0
-        total_recall = 0.0
-        total_ACLT = 0.0
-        # total_ACST = 0.0
-        total_GAP_vs_GAP = []
-        total_ARP = 0.0
-        total_ave_PL = 0.0
+        file = open(file_location + data_strategy + "_" + mlp + ".pkl", "rb")
+        result = pkl.load(file)
+        results.append(result)
+    index = pd.MultiIndex.from_product(
+        [mlp_values],
+        names=["Network layers"],
+    ).drop_duplicates()
+    results = pd.DataFrame(results, index=index)
+    detailed_results = []
+    for mlp in mlp_values:
+        file = open(file_location + 'detailed_per_item_'+data_strategy + "_" + mlp + ".pkl", "rb")
+        result = pkl.load(file)
+        detailed_results.append(result)
+    metrics_order = ["pop_corr", "ARP", "ave_PL", "ACLT", "AggDiv", "RMSE", "NDCG"]
+    metrics = results[metrics_order]
+    metrics = metrics.rename(
+        columns={"pop_corr": "PopCorr", "ave_PL": "PL", "ACLT": "APLT", "NDCG": "NDCG@10"}
+    )
+    metrics['RealPopCorr'] = metrics.PopCorr.apply(lambda x: x[0])
+    metrics['Significance'] = metrics.PopCorr.apply(lambda x: True if x[1]<0.005 else False)
+    metrics['PopCorr'] = metrics.RealPopCorr 
+    metrics = metrics.drop('RealPopCorr', axis=1)
 
-        for fold in tqdm(range(1,6)):
-            # Train and test files of that fold
-            train_file = 'data/'+data+'_fold_'+str(fold)+'_train.csv'
-            test_file = 'data/'+data+'_fold_'+str(fold)+'_test.csv'
-        
-            train_df = pd.read_csv(train_file)
-            test_df = pd.read_csv(test_file)
-            test_users = test_df.user.unique()
+    with open("metrics/"+algo_name+'/'+data_strategy+'/'+data_strategy+"_final_metrics.pkl", "wb") as f:
+        pkl.dump(metrics.round(3).drop('RMSE',axis=1), f)  # RMSE is irrelevant for DMF
 
-            # Recommendations
-            recs_file = final_recs_files[fold,mlp]
-            recs_df = pd.read_csv(recs_file, sep='\t', header=None)
-            recs_df.columns = ['user','item','rating']
+    # # Significance tests
+    print(mlp_values) 
+    print('Use the above to figure out significance comparisons.')
+    
+    # ## 1. Average Recommendation Popularity
 
-            # Test recommendations
-            test_recs = recs_df[recs_df.user.isin(test_users)].reset_index(drop=True)
-            test_recs_grouped = test_recs.groupby([user_col])[item_col].apply(list)
-
-            # Calculate all metrics
-            pop_bias= modelling_mf.calculate_pop_bias_per_item(all_items, item_col, user_col, predict_col, train_df, recs=test_recs) # OK
-            GAP_vs_GAP = modelling_mf.calculate_ave_pop_per_user(test_users, item_col, user_col, pop_bias, train_df, test_recs_grouped) # OK
-            #pop_corr = modelling_mf.calculate_pop_correlation(pop_bias)
-            precision, recall, ndcg = modelling_mf.calculate_topn_metrics(test_recs,test_df) # OK
-            # AggDiv = modelling_mf.evaluate_item_coverage(pop_bias["recommendation"].values)
-            ARP, ave_PL, ACLT = modelling_mf.calculate_all_pb_metrics(pop_bias, test_users, item_col, user_col, train_df, test_recs_grouped, test_recs) # OK
-            
-            total_ACLT += ACLT
-            total_ARP += ARP
-            total_ave_PL += ave_PL
-            total_GAP_vs_GAP.append(GAP_vs_GAP)
-            total_pop_bias_10 += pop_bias
-            total_precision += precision
-            total_recall += recall
-            total_ndcg += ndcg
-
-        total_precision /= 5.0
-        total_recall /= 5.0
-        total_ndcg /= 5.0
-        total_ACLT /= 5.0
-        total_ARP /= 5.0
-        total_ave_PL /= 5.0
-
-        total_GAP_vs_GAP = pd.concat(total_GAP_vs_GAP)
-        pop_biases = [total_pop_bias_10]
-        AggDiv = modelling_mf.evaluate_item_coverage(total_pop_bias_10["recommendation"].values)
-        pop_corr = modelling_mf.calculate_pop_correlation(total_pop_bias_10)
-        metrics_dict = {
-                "pop_corr": pop_corr,
-                "RMSE": 0, # irrelevant
-                "NDCG": total_ndcg,
-                "ARP": total_ARP,
-                "ave_PL": total_ave_PL,
-                "ACLT": total_ACLT,
-                "AggDiv": AggDiv,
-            }
-        modelling_mf.plot_results(
-            pop_biases.copy(),
-            total_GAP_vs_GAP,
-            algorithm,
-            0,
-            total_precision,
-            total_recall,
-            total_ndcg,
-            0,
-            total_ACLT,
-            cv=True,
-            n=10,
-            args='',
-            data_strategy=data,
-            save_plot=False,
-        )
-
-
-        # Save the results per mlp!
-        with open('metrics/'+algorithm+'/'+data+'/'+data+'_'+mlp+'.pkl', 'wb') as f:
-            pickle.dump(metrics_dict, f)
-        with open('metrics/'+algorithm+'/'+data+'/detailed_per_item_'+data+'_'+mlp+'.pkl', 'wb') as f:
-            pickle.dump(total_GAP_vs_GAP, f)
-
-        
+    print("ARP:")
+    print(mannwhitneyu_test(detailed_results))
+    
+    
+    # ## 2. Popularity Lift
+    
+    for df in detailed_results:
+        df['popularity_lift'] = (df['recommendation']-df['profile'])/df['profile']*100
+    print("PL:")
+    print(mannwhitneyu_test(detailed_results, column_name = 'popularity_lift')) 
+    
 
 
 
--- lib/widget.py
+++ lib/widget.py
@@ -1,7 +1,7 @@
 import ipywidgets as widgets
 from IPython.display import display
 from elliot.run import run_experiment
-from lib import tuning, training, processing_recommendations
+from lib import tuning, training, processing_recommendations, analysis_recommendations
 import os
 import pandas as pd
 import numpy as np
@@ -149,6 +149,56 @@ def processing_widget():
                 print(f'Dataset chosen: {selected_dataset}')
                 # Trigger your tuning process here
                 processing_recommendations.process(selected_dataset)
+    
+    # Attach the function to the radio button widget
+    rad_button.observe(on_radio_change)
+    
+    # Attach the function to the OK button click
+    ok_button.on_click(on_ok_button_clicked)
+    
+    # Display the radio button widget and the OK button
+    display(rad_button, ok_button, output_radb)
+
+
+def analysis_widget():
+    # Define the list of dataset options
+    dataset_options = ['fairbook', 'ml1m', 'epinion', 'synthetic']
+    
+    # Create a radio button widget
+    rad_button = widgets.RadioButtons(
+        options=dataset_options,
+        description='Choose dataset:',
+        disabled=False,
+        value=None
+    )
+    
+    # Create an OK button widget
+    ok_button = widgets.Button(
+        description='OK',
+        disabled=True
+    )
+    
+    output_radb = widgets.Output()
+    
+    # Variable to store the selected dataset
+    selected_dataset = None
+    
+    # Define a function to handle the selection change in the radio buttons
+    def on_radio_change(change):
+        global selected_dataset
+        if change['type'] == 'change' and change['name'] == 'value':
+            selected_dataset = change['new']
+            ok_button.disabled = False  # Enable OK button when a dataset is selected
+    
+    # Define a function to handle the OK button click
+    def on_ok_button_clicked(b):
+        global selected_dataset
+        with output_radb:
+            output_radb.clear_output()  # Clear previous output
+            if selected_dataset:
+                print(f'Dataset chosen: {selected_dataset}')
+                # Trigger your tuning process here
+                analysis_recommendations.analyse(selected_dataset)
     
     # Attach the function to the radio button widget
     rad_button.observe(on_radio_change)
